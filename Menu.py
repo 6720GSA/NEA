67e97f5ed24e0929c9626b5e09819fea6e1b2ec2
owen <143509643+6720GSA@users.noreply.github.com>
2024-12-05 12:43:44 +0000

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -6,7 +6,7 @@ x_mid = swidth / 2
 y_mid = sheight / 2
 pygame.display.set_caption("Physics simulations")
 
-screen = pygame.display.set_mode((swidth,sheight))
+screen = pygame.display.set_mode((swidth, sheight))
 
 dgrey = (47, 54, 52)
 grey = (100, 100, 100)
@@ -17,38 +17,47 @@ red = (200, 0, 0)
 background_blue = (0, 5, 86)
 blue = (0, 117, 200)
 orange = (255, 127, 0)
-purple = (76,0,153)
+purple = (76, 0, 153)
 
-def hexagon(x,y,scale,colour1,colour2): # creates a hexagonal button
-    multi1 = scale*10
-    multi2 = scale*5
-    multi3 = scale*11
-    p1 = (x+multi1,y+multi2)
-    p2 = (x+multi1,y-multi2)
-    p3 = (x,y-multi3)
-    p4 = (x-multi1,y-multi2)
-    p5 = (x-multi1,y+multi2)
-    p6 = (x,y+multi3)
-    pygame.draw.polygon(screen,colour1,[p1,p2,p3,p4,p5,p6],0)
-    pygame.draw.polygon(screen, colour2, [p1, p2, p3, p4, p5, p6], 4)
 
-def rectangle(width,height,x,y,centre):
-    w = width * swidth/10
-    h = height * sheight/10
-    x = x * swidth/10
-    y = y * sheight/10
+def hexagon(x, y, scale):  # creates a hexagonal button
+    multi1 = scale * 10
+    multi2 = scale * 5
+    multi3 = scale * 11
+    p1 = (x + multi1, y + multi2)
+    p2 = (x + multi1, y - multi2)
+    p3 = (x, y - multi3)
+    p4 = (x - multi1, y - multi2)
+    p5 = (x - multi1, y + multi2)
+    p6 = (x, y + multi3)
+    points = [p1, p2, p3, p4, p5, p6]
+    return points
+
+
+def poly_draw(colour1, colour2, points, ):
+    pygame.draw.polygon(screen, colour1, points, 0)
+    pygame.draw.polygon(screen, colour2, points, 4)
+
+
+def rectangle(width, height, x, y, centre):
+    w = width * swidth / 10
+    h = height * sheight / 10
+    x = x * swidth / 10
+    y = y * sheight / 10
     if centre == True:
         x = x_mid - (w / 2)
     return (x, y, w, h)
 
-def pos_check(button_poly):
-    mousex,mousey = pygame.mouse.get_pos()
-    return button_poly.collidepoint(mousex, mousey)
 
+def pos_check(button):
+    return button.collidepoint(pygame.mouse.get_pos())
+
+
+#def poly_check(button):
+#   points =
 
 running = True
 current_screen = ["menu"]
-
 
 while running:
     events = pygame.event.get()
@@ -60,16 +69,22 @@ while running:
     pygame.display.update()
     screen.fill(black)
     if current_screen[-1] != "login":
-        header = pygame.draw.rect(screen,dgrey,rectangle(10,1,0,0,False))
-        title_box = pygame.draw.rect(screen,grey,rectangle(2,0.45,4,0.25,True))
+        header = pygame.draw.rect(screen, dgrey,
+                                  rectangle(10, 1, 0, 0, False))  #creates the bar at the top of the screen
+        title_box = pygame.draw.rect(screen, grey, rectangle(2, 0.45, 4, 0.25,
+                                                             True))  # creates the title box at the top of the screen
+        back_button = pygame.draw.rect(screen, purple, rectangle(1, 0.4, 0.25, 0.25, False))  # creates the back button
+        if pos_check(back_button):
+            back_button = pygame.draw.rect(screen, orange, rectangle(1, 0.4, 0.25, 0.25,
+                                                                     False))  # changes the colour of the back button when hovered
 
-    if current_screen[-1] == "menu":
-        chrg_button = hexagon(380,350,20,purple,blue)
-        mass_button = hexagon(580,670,20,purple,blue)
-        wave_button = hexagon(780,350,20,purple,blue)
+    if current_screen[-1] == "menu":  # defines the polygons on the menu screen
+        chrg_button_points = hexagon(380,350,20)
+        chrg_button = poly_draw(purple,blue,chrg_button_points)
+        mass_button_points = hexagon(580, 670, 20)
+        mass_button = poly_draw(purple,blue,mass_button_points)
+        wave_button_points = hexagon(780, 350, 20)
+        wave_button = poly_draw(purple,blue,wave_button_points)
 
-        if pos_check(chrg_button):
-            chrg_button = hexagon(380,350,20,orange,blue)
-
-
-
+        #if pos_check(chrg_button):
+         #   chrg_button = hexagon(380, 350, 20, orange, blue)
